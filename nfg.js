4b875008c1320b41d0d0d11c0b8d1caa8b140800
Lokus <114109127+dudu-cash1@users.noreply.github.com>
2023-02-03 20:04:25 +0200

    Create nfg.js

---
+++ nfg.js
@@ -0,0 +1,4 @@
+     )
+        cprint(f">>> Успешно | {address} | {amount_to_withdrawal}", "green")
+    except Exception as error:
+        cprint(f">>> Неудачно | {address} | ошибка : {error}", "red")
